4dd9b3d6d5a43477c3c0d067268a07e704a31066
AnasKhan44 <anaskhan.ak2005@gmail.com>
2024-04-11 00:59:49 +0500

    Enhanced UI

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
 #! /usr/bin/env node
 import inquirer from "inquirer";
+import chalk from "chalk";
 let myBal = 10000;
-let myPin = 5131;
-const ATM = await inquirer.prompt([{ message: "Enter Your Pin: ", type: 'number', name: 'pin' }]);
+let myPin = 1234;
+const ATM = await inquirer.prompt([{ message: "****************** Enter Your Pin ******************\n", type: 'number', name: 'pin' }]);
 if (ATM.pin === myPin) {
-    console.log(`Success!`);
-    let operation = await inquirer.prompt([{ message: "Select Operation", type: "list", name: "oper", choices: ["Deposit", "Withdrawal", "Fastcash"] }]);
+    console.log(chalk.bold(`${chalk.cyan(`\n  ****************** Success! ******************\n`)}`));
+    let operation = await inquirer.prompt([{ message: chalk.yellow("****************** Select Operation ******************\n"), type: "list", name: "oper", choices: ["Deposit", "Withdrawal", "Fastcash"] }]);
     if (operation.oper === "Withdrawal") {
-        let amount = await inquirer.prompt({ message: `Enter Amount: `, type: 'number', name: 'sum' });
+        console.log("\n");
+        let amount = await inquirer.prompt({ message: chalk.green(`****************** Enter Amount: ******************\n`), type: 'number', name: 'sum' });
         if (myBal - amount.sum < 0) {
-            console.log('Insufficient Funds');
+            console.log(chalk.red(chalk.bold(`****************** Insufficient Funds ******************`)));
             process.exit();
         }
         else {
@@ -18,15 +20,17 @@ if (ATM.pin === myPin) {
         }
     }
     else if (operation.oper === "Fastcash") {
-        let fastOp = await inquirer.prompt({ message: "Options: ", type: "list", choices: ["1000", '2000', '5000', '10000'], name: "choices" });
+        console.log("\n");
+        let fastOp = await inquirer.prompt({ message: chalk.green("****************** Options ******************"), type: "list", choices: ["1000", '2000', '5000', '10000'], name: "choices" });
         console.log(`Current Bal: ${myBal - fastOp.choices}`);
     }
     else {
-        let amount = await inquirer.prompt({ message: `Enter Amount: `, type: 'number', name: 'sum' });
+        console.log("\n");
+        let amount = await inquirer.prompt({ message: chalk.green(`****************** Enter Amount ******************\n`), type: 'number', name: 'sum' });
         myBal += amount.sum;
         console.log(`Current Bal: ${myBal}`);
     }
 }
 else {
-    console.log("Wrong Pin Entered");
+    console.log(chalk.bold("\n******************Wrong Pin Entered******************"));
 }
